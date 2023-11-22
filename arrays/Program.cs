6dcf4c82565f14570b6ccec972be2eb1e7521a3c
Allandpn <99135710+Allandpn@users.noreply.github.com>
2023-11-22 20:04:58 -0300

    create

---
+++ arrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography;
+using System;
+
+
+public class Ponto {
+    public int x {get; set;}
+    public int y {get; set;}
+}
+
+
+
+class MainClass {
+
+    const int TAM = 100;
+    
+    public static void Main(string[] args) {
+    
+        Ponto[] pontos = new Ponto[TAM];
+        
+        for(int i = 0; i < TAM; i++){
+        Ponto pt = new Ponto {x = new Random().Next(100, 300), y = new Random().Next(500,800)};
+        pontos[i] = pt;
+        }
+              
+
+        
+        Console.WriteLine(Array.Find(pontos, p => p.x * p.y > 100500));
+
+        Ponto[] result =  Array.FindAll(pontos, p => p.x * p.y > 200500);
+
+        foreach (Ponto p in result)
+        {
+            Console.WriteLine("Meus pontos: {0}, {1}",p.x, p.y);
+        }
+        
+
+    }
+}
